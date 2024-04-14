a5864dec396aabf2d8bd1313d8019e1946bc1665
gnafhan <nafhanghifari@gmail.com>
2024-04-14 16:35:29 +0700

    godot failed

---
+++ src/app/godot/page.jsx
@@ -0,0 +1,13 @@
+// import ReactGodot from 'react-godot'
+
+import Image from 'next/image'
+
+const Godot = () => {
+  return (
+    <div>
+      <Image src='/assets/bg.png' alt='First' width={500} height={500} />
+    </div>
+  )
+}
+
+export default Godot
